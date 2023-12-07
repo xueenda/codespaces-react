1bcd349430dae439799ee0a1d60645372b73cbd1
Enda Xue <xueenda@gmail.com>
2023-12-07 03:10:28 +0000

    Checkin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: { https: true },
+  plugins: [mkcert(), react()],
   test: {
     globals: true,
     environment: 'jsdom',
   },
 })
+
